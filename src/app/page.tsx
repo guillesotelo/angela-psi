19b3e6559a7cf9656435820012d1653785dbca11
Guillermo Sotelo <guille.sotelo.cloud@gmail.com>
2025-03-23 22:31:10 +0100

    all

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+import { Metadata } from 'next'
+import { cache } from 'react'
+import Home from './home/page'
+
+export const metadata: Metadata = {
+    metadataBase: new URL(process.env.NODE_ENV === 'production' ? 'https://angela-psinews.com' : 'http://localhost:3000'),
+    title: 'angela-psi.',
+    description: '',
+    openGraph: {
+        title: '',
+        description: '',
+        images: ['/logo_515x515.png'],
+        url: 'https://www.angela-psinews.com',
+        type: 'website',
+    },
+    twitter: {
+    },
+}
+
+const HomePage = async () => {)
+
+    return <Home />
+}
+
+export default HomePage
